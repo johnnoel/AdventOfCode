81b08d1003a2f28990bcbd7cfc4698f88c7cd401
John Noel <john.noel@plus.net>
2019-02-01 14:57:58 +0000

    Day 06 - Part 2

--- 2018/day06.php
+++ 2018/day06.php
@@ -17,56 +17,18 @@ $maxY = array_reduce($coords, function($acc, $item) {
     return max($acc, $item[1]);
 }, 0);
 
-// which grid points are owned by what coord
-$gridOwnership = [];
+$tenKOrLess = [];
 
 for ($y = 0; $y <= $maxY; $y++) {
     for ($x = 0; $x <= $maxX; $x++) {
-        $pointDistances = [];
+        $totalDistance = array_reduce($coords, function($acc, $item) use ($x, $y) {
+            return $acc + manhattanDistance([ $x, $y ], $item);
+        }, 0);
 
-        foreach ($coords as $k => $point) {
-            $distance = manhattanDistance([ $x, $y ], $point);
-
-            if (!array_key_exists($distance, $pointDistances)) {
-                $pointDistances[$distance] = [];
-            }
-
-            $pointDistances[$distance][] = $k;
-        }
-
-        $minDist = min(array_keys($pointDistances));
-        if (count($pointDistances[$minDist]) > 1) {
-            continue;
-        }
-
-        $gridOwnership[$x][$y] = $pointDistances[$minDist][0];
-    }
-}
-
-$areas = array_fill(0, 50, 0);
-$infiniteAreas = [];
-
-for ($y = 0; $y <= $maxY; $y++) {
-    for ($x = 0; $x <= $maxX; $x++) {
-
-        if (isset($gridOwnership[$x][$y])) {
-            $coordKey = $gridOwnership[$x][$y];
-            $areas[$coordKey]++;
-
-            if ($x === 0 || $y === 0 || $x === $maxX || $y === $maxY) {
-                $infiniteAreas[] = $coordKey;
-            }
+        if ($totalDistance < 10000) {
+            $tenKOrLess[] = [ $x, $y ];
         }
     }
 }
 
-$infiniteAreas = array_unique($infiniteAreas);
-
-arsort($areas, SORT_NUMERIC);
-
-foreach ($areas as $coordKey => $area) {
-    if (!in_array($coordKey, $infiniteAreas)) {
-        var_dump($coords[$coordKey], $area);
-        break;
-    }
-}
+var_dump(count($tenKOrLess));
