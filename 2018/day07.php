addc55969db53b0432c099a25879c292c08d6fa2
John Noel <john.noel@plus.net>
2019-02-04 11:32:32 +0000

    Day 07 - part 2

--- 2018/day07.php
+++ 2018/day07.php
@@ -4,7 +4,7 @@ $instructions = array_filter(explode("\n", file_get_contents(__DIR__.'/inputs/07
 $regex = '/^Step ([A-Z]) must be finished before step ([A-Z]) can begin.$/';
 
 // key = step, value = steps that must be completed before key can start
-$map = [];
+$map = array_combine(range('A', 'Z'), array_fill(0, 26, []));
 
 foreach ($instructions as $inst) {
     $matches = [];
@@ -17,24 +17,37 @@ foreach ($instructions as $inst) {
     $mustFinish = $matches[1];
     $step = $matches[2];
 
-    if (!array_key_exists($step, $map)) {
-        $map[$step] = [];
-    }
-
     $map[$step][] = $mustFinish;
 }
 
-// which steps have we seen, but don't have any pre-requisites to start?
-$availableSteps = array_diff(range('A', 'Z'), array_keys($map));
+// key = second, value = [ steps ]
+$workLog = [];
+// key = step, value = time spent
+$currentWork = [];
+// value = step
+$availableSteps = [];
 $performed = [];
+// key = step, value = time cost
+$stepTimes = array_combine(range('A', 'Z'), range(61, 86));
 
-while (true) {
-    sort($availableSteps);
-    $performed[] = array_shift($availableSteps);
+$workers = 5;
 
+for ($second = 0; ; $second++) {
+    // filter out any steps that have completed
+    foreach ($currentWork as $step => &$timeSpent) {
+        if ($timeSpent === $stepTimes[$step]) {
+            unset($currentWork[$step]);
+            $performed[] = $step;
+            continue;
+        }
+
+        $timeSpent++;
+    }
+
+    // get the steps available to perform
     foreach ($map as $step => $preReqs) {
-        // if we've already done it or it's already possible
-        if (in_array($step, $performed) || in_array($step, $availableSteps)) {
+        // if we've already done it, or it's already possible, or working on it
+        if (in_array($step, $performed) || in_array($step, $availableSteps) || array_key_exists($step, $currentWork)) {
             continue;
         }
 
@@ -48,7 +61,26 @@ while (true) {
         $availableSteps[] = $step;
     }
 
-    if (count($availableSteps) === 0) {
+    sort($availableSteps);
+
+    // all workers occupied
+    if (count($currentWork) === $workers) {
+        continue;
+    }
+
+    while (count($currentWork) < $workers) {
+        if (count($availableSteps) > 0) {
+            $currentWork[array_shift($availableSteps)] = 1;
+        } else {
+            break;
+        }
+    }
+
+    $workLog[$second] = $currentWork;
+
+    echo sprintf('%d - %s', $second, implode(', ', array_keys($currentWork))).PHP_EOL;
+
+    if (count($availableSteps) === 0 && count($currentWork) === 0) {
         if (count($performed) === 26) {
             echo 'Success: '.implode(' -> ', $performed).PHP_EOL;
             echo implode($performed).PHP_EOL;
