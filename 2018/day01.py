47121e2b37ae2ffefcb49790e9ed410b973f36bc
John Noel <chaostangent@gmail.com>
2019-02-14 20:22:02 +0000

    Day 01 - part 2 (Python)

--- 2018/day01.py
+++ 2018/day01.py
@@ -8,5 +8,20 @@ def parse(num):
 
 with open('inputs/01.txt', 'r') as f:
     data = [parse(x) for x in f.read().splitlines()]
-    value = functools.reduce(lambda x, y: x + y, data, 0)
-    print(value)
+    part_1 = functools.reduce(lambda x, y: x + y, data, 0)
+
+    freqs = set()
+    part_2 = None
+    current = 0
+
+    while part_2 is None:
+        for freq in data:
+            current += freq
+
+            if current in freqs:
+                part_2 = current
+                break
+
+            freqs.add(current)
+
+    print(part_1, part_2)
