9a9ade37eaac99081c8f59973066a01e6c6458c5
John Noel <chaostangent@gmail.com>
2019-02-16 09:17:24 +0000

    Day 03 - part 2 (Python)

--- 2018/day03.py
+++ 2018/day03.py
@@ -7,9 +7,15 @@ def parse(line):
     m = re.search('^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$', line)
     return Fabric(id=int(m.group(1)), x=int(m.group(2)), y=int(m.group(3)), w=int(m.group(4)), h=int(m.group(5)))
 
+def intersects(f1, f2):
+    return not (f1.x > f2.x + f2.w or
+            f1.x + f1.w < f2.x or
+            f1.y > f2.y + f2.h or
+            f1.y + f1.h < f2.y)
+
 with open('inputs/03.txt', 'r') as f:
     lines = f.read().splitlines()
-    fabrics = [parse(line) for line in lines]
+    fabrics = {parse(line) for line in lines}
 
     min_x = min([f.x for f in fabrics])
     min_y = min([f.y for f in fabrics])
@@ -25,4 +31,16 @@ with open('inputs/03.txt', 'r') as f:
 
     part_1 = sum([ sum([ 1 if count > 1 else 0 for count in inner ]) for inner in grid ])
 
-    print(part_1, a)
+    collisions = set()
+    for f1 in fabrics:
+        for f2 in fabrics:
+            if f1 == f2 or f2 in collisions:
+                continue
+
+            if intersects(f1, f2):
+                collisions.add(f1)
+                collisions.add(f2)
+
+    part_2 = fabrics - collisions
+
+    print(part_1, part_2)
