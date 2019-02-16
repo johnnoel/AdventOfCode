e4558f62cecb6ff60f190272f191ff517fddc0db
John Noel <chaostangent@gmail.com>
2019-02-16 11:53:29 +0000

    Single loop optimisation for day 04 - part 1 (Python)

--- 2018/day04.py
+++ 2018/day04.py
@@ -35,23 +35,16 @@ with open('inputs/04.txt', 'r') as f:
     logs.sort(key=lambda v: v.datetime)
 
     current_guard = None
-    guard_ids = set()
-
-    for log in logs:
-        if log.start:
-            current_guard = log.guard_id
-
-        log.guard_id = current_guard
-        guard_ids.add(log.guard_id)
-
-    guards = {guard_id: Guard(guard_id, 0, [0 for i in range(0, 60)]) for guard_id in guard_ids}
-
     asleep = None
-    current_guard = None
+    guards = {}
 
     for log in logs:
         if log.start:
-            current_guard = guards[log.guard_id]
+            if log.guard_id not in guards:
+                current_guard = Guard(log.guard_id, 0, [0 for i in range(0, 60)])
+                guards[log.guard_id] = current_guard
+            else:
+                current_guard = guards[log.guard_id]
 
         if log.asleep:
             asleep = log.datetime
