3efa73ef9d375e56bac508c20ecfe27d3af6cec3
John Noel <chaostangent@gmail.com>
2020-08-30 12:42:55 +0100

    Day 7 Python

--- 2018/day07.py
+++ 2018/day07.py
@@ -7,8 +7,9 @@ befores = { v: set() for v in all_steps }
 for rule in rules:
     befores[rule[1]].add(rule[0])
 
+first_steps = set([ k for k in befores if len(befores[k]) == 0 ])
 performed = []
-next_steps = set([ k for k in befores if len(befores[k]) == 0 ])
+next_steps = first_steps.copy()
 
 while len(performed) < 26:
     performed.append(sorted(next_steps)[0])
@@ -17,4 +18,26 @@ while len(performed) < 26:
 
 part_1 = ''.join(performed)
 
-print(part_1)
+workers = 6
+work = set()
+second = 0
+performed = []
+next_steps = first_steps.copy()
+
+while len(performed) < 26:
+    new_work = set([ w for w in work if second < (w[1] + (ord(w[0]) - 4)) ])
+    performed.extend([ w[0] for w in work - new_work ])
+    work = new_work
+
+    if len(work) < workers:
+        work |= set([ (step, second) for step in sorted(next_steps)[:(workers - len(work))] ])
+
+    p = set(performed)
+    #next_steps = set([ k for k in befores if len(befores[k] - p) == 0]) - p
+    for k in befores:
+
+    second += 1
+
+part_2 = second
+
+print(part_1, second)
