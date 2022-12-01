e34acf56c1a6f5ee3480760a04604ad4730e9c1b
John Noel <john@evoluted.email>
2022-12-01 08:17:23 +0000

    2022 Day 01 - part 2

---
+++ 2022/day01-b.php
@@ -0,0 +1,10 @@
+<?php
+
+$elves = explode("\n\n", trim(file_get_contents(__DIR__ . '/day01.txt')));
+$elfCalories = array_map(function (string $e): int {
+    return array_sum(array_map('intval', explode("\n", $e)));
+}, $elves);
+
+rsort($elfCalories);
+
+echo array_sum(array_slice($elfCalories, 0, 3));
